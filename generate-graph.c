29208b6bc317043d9aec5139300e546ef443fa53
Trever Hall <treahall@ut.utm.edu>
2022-04-21 13:40:44 -0500

    adding starter C file.

---
+++ generate-graph.c
@@ -0,0 +1,13 @@
+// Author: Trever Hall
+// Dates: start - 4/21/2022 & last update - 4/21/2022
+// Description: generates a graph (PDF) of the working directory tree.
+
+#include <stdlib.h>
+
+// ========================================================================= //
+// MAIN FUNCTION
+// 
+int main(int argc, char *argv[]) 
+{
+    
+}
