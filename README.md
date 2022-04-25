e1367722a55735fca9d15900deffb3f62f700465
Treahall <treahall@ut.utm.edu>
2022-04-24 19:55:25 -0500

    Refactored, polished, and documented code.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+
 # I-node Graph Generator 
+
+### Description
+
+C program that creates a GraphViz formatted file called "graph.gv" that represents the relationships of a directory. This GraphViz file can then be used to create a PDF using DOT. 
+
+### How To Run (in Linux Terminal):
+
+Files Needed (in the same directory):
+- generate-graph.c
+- makefile
+
+Steps to Make PDF for Directory of the Executable:
+1. Run the 'make' command from the Terminal.
+    - This generates the executable.
+    - Then it runs the executable with no arguments (which defaults the directory to the working directory of the executable).
+    - Then it uses DOT to generate a PDF called 'graph.pdf'.
+
+Steps to make PDF of a Directory you Specify:
+1. Run the 'make' command from the Terminal.
+    - This does all the above.
+    - If you have already done this, then you can skip step 1.
+1. Run the executable with a single argument, the directory to graph.
+    - ./generate-graph \[path].
+1. Rerun the 'make' command.
+    - This will recognize that the graph.gv file has changed, and use DOT to remake the PDF.
+    - Alternatively, you can run 'dot -Tpdf:cairo graph.gv -o graph.pdf'.
--- generate-graph.c
+++ generate-graph.c
@@ -1,5 +1,5 @@
 // Author: Trever Hall
-// Dates: start - 4/21/2022 & last update - 4/23/2022
+// Dates: start - 4/21/2022 & last update - 4/24/2022
 // Description: generates a .gv file of a directory tree.
 
 #include <stdlib.h>
@@ -11,99 +11,128 @@
 #include <string.h>        // sprintf().
 #include <unistd.h>        // getcwd().
 
-int GV_NODE_COUNT =  0;
-struct stat node;
+/// Globals used to simplify function parameters, and overall code. ///
+// Used to give each node a unique number in the .gv file.
+int GV_NODE_COUNT = 0;
+// Struct used to store the current nodes information.
+struct stat NODE;
 
-// ========================================================================= //
-//HELPER FUNCTIONS
-void add_node(FILE *fp, char *path){
+
+/// ======================================================================= ///
+// HELPER FUNCTIONS //
+
+// Adds a node to the .gv file for the given path.
+void add_node(FILE *fp, char *path) {
   fprintf(fp, "\t%i [label = <%s <br/> \n\tFile size: %jd <br/> \n\tBlocks Allocated: %jd <br/> \n\tI-node Number: %ju <br/> \n\tLink Count: %ju <br/>>];\n",
-    GV_NODE_COUNT, path, (intmax_t) node.st_size,  (intmax_t) node.st_blocks,
-    (intmax_t) node.st_ino, (intmax_t) node.st_nlink);
+    GV_NODE_COUNT, path, (intmax_t) NODE.st_size,  (intmax_t) NODE.st_blocks,
+    (intmax_t) NODE.st_ino, (intmax_t) NODE.st_nlink);
 }
 
 
+
+// Recursively writes all the directory's contents as nodes
+// and relationships to a .gv file. 
 void write_directory_to_file(FILE *fp, char *directory)
 {
+  // Log number representing this directory in the .gv file. 
   int current_dir_id = GV_NODE_COUNT;
-  // Write directory entries to .gv file. //
-  DIR *d = opendir(directory); // open the directory.
-  struct dirent *dentry; // directory entry data structure.
+  // Open the directory for reading.
+  DIR *d = opendir(directory);
+  // Structure used to store current directory entry's information.
+  struct dirent *dentry; 
 
-  while ((dentry = readdir(d)) != NULL) // if there is something in the directory.
-  {
+  // If not empty, read an entry from the directory into dentry.
+  while ((dentry = readdir(d)) != NULL) {
+    // Increment count to give it a unique node number.
     GV_NODE_COUNT++;
-    // Append entry name to directory path.
+
+    // Get name of new path.
     char new_path[255];
     sprintf(new_path, "%s/%s", directory, dentry->d_name);
-    // verify lstat works for new path.
-    if (lstat(new_path, &node) == -1)
-    {
+
+    // Load the nodes stats, into global variable NODE.
+    if (lstat(new_path, &NODE) == -1) {
       perror("lstat");
       exit(EXIT_FAILURE);
     }
-    // Add node and directory->entry relationship to .gv file. 
+
+    // Add node and add directory->entry relationship to .gv file. 
     add_node(fp, new_path);
     fprintf(fp, "\t%i -> %i\n\n", current_dir_id, GV_NODE_COUNT);
 
-    // If directory, recursively write its contents. (excludes . and ..)
+    // If dentry is a directory, 
+    // then recursively write its contents (excluding . and ..).
     if(dentry->d_type == DT_DIR 
       && strcmp(dentry->d_name,".") != 0 
-      && strcmp(dentry->d_name,"..") != 0) 
-    {
+      && strcmp(dentry->d_name,"..") != 0) {
       write_directory_to_file(fp, new_path);
     }
   }
 
+  // Close the directory.
   closedir(d);
 }
 
 
+
+// Writes a DOT-readable GraphViz file (graph.gv) of a given directory.
 void write_GV(char *directory)
 {
+  // Verify lstat works for (in this context) the root directory.
+  if (lstat(directory, &NODE) == -1)
+  {
+    perror("lstat");
+    exit(EXIT_FAILURE);
+  }
+
+  // Init a FILE pointer.
   FILE *fp;
-  // Open the file.
+  // Open the file for re-writing.
   fp = fopen("graph.gv", "w+");
+
   // Print beginning of the .gv file
   fprintf(fp, "digraph {\n");
   fprintf(fp, "\trankdir=LR\n\n");
   fprintf(fp, "\t/* Relationships */\n");
-  // Add root (in this context) directory information to .gv file.
+
+  // Add (in this context) root directory's information to .gv file.
   add_node(fp, directory);
-  // Add a new line for human-readability.
+  // Add a new line to improve human-readability.
   fprintf(fp, "\n");
+
   // Recursively write all contents of the directory.
   write_directory_to_file(fp, directory);
+
   // Print the closing bracket of the file.
   fprintf(fp, "}");
+
   // Close the file.
   fclose(fp);
 }
 
-// ========================================================================= //
-// MAIN FUNCTION - ENRTY POINT
+
+
+/// ======================================================================= ///
+// MAIN FUNCTION - ENRTY POINT //
+
 int main(int argc, char *argv[])
 {
-  // Handle arguments.
+  // Handle arguments to decide directory used. //
   char *directory;
   if(argc == 1){
+    //Used directory the executable is called from.
     directory = getcwd(NULL, 0);
   }
   else if(argc == 2){
+    //Use the directory given as a single argument.
     directory = argv[1];
   }
   else{
     perror("Too many argments.");
     exit(EXIT_FAILURE);
   }
-
-  // Verify lstat works for root (in this context) directory.
-  if (lstat(directory, &node) == -1)
-  {
-    perror("lstat");
-    exit(EXIT_FAILURE);
-  }
   
+  // Call function that writes the i-node graph to "graph.gv".
   write_GV(directory);
   // Clean up allocated memory.
   free(directory);
